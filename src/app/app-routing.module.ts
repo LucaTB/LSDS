57a892d5387cf214963001accd685f2076753dd3
Luca T.B <lucatourn@gmail.com>
2022-05-11 13:38:02 -0300

    Kickoff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from 'src/app/components/pages/home/home.component';
+import { FechasComponent } from 'src/app/components/pages/fechas/fechas.component';
+import { BandsComponent } from 'src/app/components/pages/bands/bands.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'fechas',
+    component: FechasComponent,
+  },
+  {
+    path: 'bands',
+    component: BandsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompNavbarComponent } from './components/comp-navbar/comp-navbar.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { FechasComponent } from './components/pages/fechas/fechas.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BandsComponent } from './components/pages/bands/bands.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompNavbarComponent,
+    HomeComponent,
+    FechasComponent,
+    FooterComponent,
+    BandsComponent
   ],
   imports: [
     BrowserModule,
